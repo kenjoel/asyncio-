5c5314f644c81c74fb282d349bbdba0c60834343
kenjoel <jrjoemuigai@gmail.com>
2021-12-13 09:02:08 +0300

    gitignore added

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,11 @@ from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+from backend.app.config import settings
+from backend.app.models.models import Base
+
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +21,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
